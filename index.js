cd76c7add337b73b6c7fcf93efdf172327e6fb08
Nefton <neftonjin@yahoo.com>
2023-03-19 14:58:37 +0000

    added render method

--- index.js
+++ index.js
@@ -14,31 +14,150 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
   
-inquirer.prompt([{
-            type: "input",
-            name: "name",
-            message: "What is the team manager`s name?",
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "What is the team manager`s employee ID?",
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "What is the team manager`s email address?",
-    
-        },
-        {
-            type: "input",
-            name: "officeNr",
-            message: "What is the team manager`s office number?",
-        }
-       
-    ])
-    .then((response) => {
-   console.log(response);
-   console.log(response.email);
+const employees = [];
 
-})
+function addManager() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: "Enter the team manager's name:",
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: "Enter the team manager's employee ID:",
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: "Enter the team manager's email address:",
+      },
+      {
+        type: 'input',
+        name: 'officeNumber',
+        message: "Enter the team manager's office number:",
+      },
+    ])
+    .then((answers) => {
+      const manager = new Manager(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.officeNumber
+      );
+      employees.push(manager);
+      menu();
+    });
+}
+//adding engineer
+function addEngineer() {
+    inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'name',
+          message: "Enter the engineer's name:",
+        },
+        {
+          type: 'input',
+          name: 'id',
+          message: "Enter the engineer's employee ID:",
+        },
+        {
+          type: 'input',
+          name: 'email',
+          message: "Enter the engineer's email address:",
+        },
+        {
+          type: 'input',
+          name: 'github',
+          message: "Enter the engineer's GitHub username:",
+        },
+      ])
+      .then((answers) => {
+        const engineer = new Engineer(
+          answers.name,
+          answers.id,
+          answers.email,
+          answers.github
+        );
+        employees.push(engineer);
+        menu();
+      });
+  }
+// adding intern
+function addIntern() {
+    inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'name',
+          message: "Enter the intern's name:",
+        },
+        {
+          type: 'input',
+          name: 'id',
+          message: "Enter the intern's employee ID:",
+        },
+        {
+          type: 'input',
+          name: 'email',
+          message: "Enter the intern's email address:",
+        },
+        {
+          type: 'input',
+          name: 'school',
+          message: "Enter the intern's school:",
+        },
+      ])
+      .then((answers) => {
+        const intern = new Intern(
+          answers.name,
+          answers.id,
+          answers.email,
+          answers.school
+        );
+        employees.push(intern);
+        menu();
+      });
+  }
+
+//menu function
+function menu() {
+    inquirer
+      .prompt([
+        {
+          type: 'list',
+          name: 'choice',
+          message: 'What would you like to do?',
+          choices: ['Add an engineer', 'Add an intern', 'Finish building the team'],
+        },
+      ])
+      .then((answers) => {
+        switch (answers.choice) {
+          case 'Add an engineer':
+            addEngineer();
+            break;
+          case 'Add an intern':
+            addIntern();
+            break;
+          case 'Finish building the team':
+            finish();
+            break;
+        }
+      });
+  }
+//Finish function
+function finish() {
+    const html = render(employees);
+    if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR);
+      }
+    const outputPath = './output/team.html';
+    fs.writeFileSync(outputPath, html);
+    console.log(`HTML file written to ${outputPath}`);
+  }
+
+  addManager();
